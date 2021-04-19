7d273413bf1a3ffa1305d3d400c73291de1e400a
Darren Henderson <darrenhusa@yahoo.com>
2021-04-19 17:59:40 -0500

    Add trial formatting to dashboard view.

--- resources/views/livewire/trad-dashboard.blade.php
+++ resources/views/livewire/trad-dashboard.blade.php
@@ -1,24 +1,31 @@
 <div wire:poll.60s="getDashboardData">
     <p>Last updated on <strong>{{ $date_timestamp }}</strong></p>
-    <h1>TRAD Programs by Entry Type</h1>
+
+    <div class="container mx-auto flex" style="max-width:1000px">
+        <div>
+            <h1>TRAD Programs by Entry Type</h1>
+            <table border=1>
+                <tr>
+                <thead>
+                <th>Entry Type</th><th>FT N</th><th>PT N</th><th>FT Cr Hrs</th><th>PT Cr Hrs</th>
+                </thead>
+                </tr>
+                    <tr><td>Continuing</td><td>{{ $ft_trad_continuing }}</td><td>{{ $pt_trad_continuing }}</td><td>{{ $ft_trad_continuing_cr_hrs}}</td><td>{{ $pt_trad_continuing_cr_hrs}}</td>
+                </tr>
+                <tr><td>First-time</td><td>{{ $ft_trad_firsttime }}</td><td>{{ $pt_trad_firsttime }}</td><td>{{ $ft_trad_firsttime_cr_hrs}}</td><td>{{ $pt_trad_firsttime_cr_hrs}}</td>
+                </tr>
+                <tr><td>Transfer</td><td>{{ $ft_trad_transfer }}</td><td>{{ $pt_trad_transfer }}</td><td>{{ $ft_trad_transfer_cr_hrs}}</td><td>{{ $pt_trad_transfer_cr_hrs}}</td>
+                </tr>
+                <tr><td>Totals</td><td><strong>{{ $ft_trad_total }}</strong></td><td>{{ $pt_trad_total }}</td><td>{{ $ft_trad_total_cr_hrs}}</td><td>{{ $pt_trad_total_cr_hrs}}</td>
+                </tr>
+                </table>            
+        </div>
+        
+    </div>
+    <div class="container mx-auto flex" style="max-width:1000px">
+        <div style="max-width:300px">
+        <h1>TRAD FullTime by Class Division</h1>
     <table border=1>
-    <tr>
-    <thead>
-    <th>Entry Type</th><th>FT N</th><th>PT N</th><th>FT Cr Hrs</th><th>PT Cr Hrs</th>
-    </thead>
-    </tr>
-        <tr><td>Continuing</td><td>{{ $ft_trad_continuing }}</td><td>{{ $pt_trad_continuing }}</td><td>{{ $ft_trad_continuing_cr_hrs}}</td><td>{{ $pt_trad_continuing_cr_hrs}}</td>
-    </tr>
-    <tr><td>First-time</td><td>{{ $ft_trad_firsttime }}</td><td>{{ $pt_trad_firsttime }}</td><td>{{ $ft_trad_firsttime_cr_hrs}}</td><td>{{ $pt_trad_firsttime_cr_hrs}}</td>
-    </tr>
-    <tr><td>Transfer</td><td>{{ $ft_trad_transfer }}</td><td>{{ $pt_trad_transfer }}</td><td>{{ $ft_trad_transfer_cr_hrs}}</td><td>{{ $pt_trad_transfer_cr_hrs}}</td>
-    </tr>
-    <tr><td>Totals</td><td><strong>{{ $ft_trad_total }}</strong></td><td>{{ $pt_trad_total }}</td><td>{{ $ft_trad_total_cr_hrs}}</td><td>{{ $pt_trad_total_cr_hrs}}</td>
-    </tr>
-    </table>
-    
-    <h1>TRAD FullTime by Class Division</h1>
-    <table>
     <tr>
         <thead>
             <th>Class</th>
@@ -58,26 +65,27 @@
         <td>TOTAL</td>
         <td><strong>{{ $ft_trad_cdiv_total}}</strong></td>
     </tr>
+    </table>    
+        </div>
 
-    </table>
-</div>
+        <div style="max-width:700px">
+        <h1>TRAD FullTime By Top Majors</h1>
+            <table border=1>
+            <thead>
+            <tr>
+                <th>Major</th><th>Count</th>
+            </tr>
+            </thead>
 
-<div>
-<h1>TRAD FullTime By Top Majors</h1>
-<table>
-<thead>
-<tr>
-    <th>Major</th><th>Count</th>
-</tr>
-</thead>
-
-    @foreach($top_majors as $key => $value)
-    <tr>
-        <td>{{ $key }}</td> <td>{{ $value }}</td>
-    </tr>
-    @endforeach
-    <tr>
-    <td>TOTAL</td><td><strong>{{ $ft_trad_majors_total }}</strong></td>
-    </tr>
-</table>
+                @foreach($top_majors as $key => $value)
+                <tr>
+                    <td>{{ $key }}</td> <td>{{ $value }}</td>
+                </tr>
+                @endforeach
+                <tr>
+                <td>TOTAL</td><td><strong>{{ $ft_trad_majors_total }}</strong></td>
+                </tr>
+            </table>  
+        </div>
+    </div>
 </div>
